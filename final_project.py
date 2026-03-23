36ac15de0a9d6b1dc911bf0c01de61282da30bef
carolina-rossman <rossman5@illinois.edu>
2026-03-23 17:50:44 -0500

    continue

--- final_project.py
+++ final_project.py
@@ -4,3 +4,21 @@
 
 # Begginging sequence, Dino runs on screen, like in jetpack joyride, 
 # once it hits the center of the screen the background begins to scroll instead on the dino moving 
+
+# background that scrolls throughout run(stops when character is killed) Patrica
+
+#Base Dino, should be able to jump when space bar is hit (Carolina)
+
+# Powerups (Carolina)
+
+# Jet pack (Carolina)
+
+# Double score tokens (Carolina)
+
+# Rocket blaster 
+
+# Power Downs (Carolina)
+
+# Half score token (Carolina)
+
+# Shorterned jumps (Carolina)
