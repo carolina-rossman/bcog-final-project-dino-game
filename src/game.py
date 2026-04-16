335f8761d0d96df8c2d1574f927d9df69dcffc62
carolina-rossman <rossman5@illinois.edu>
2026-04-16 15:22:15 -0500

    starting game.py

---
+++ src/game.py
@@ -0,0 +1,27 @@
+import tkinter as tk 
+import FinalProject_PracticeCode
+
+class Display(): 
+    def __init__(self, root): 
+        pass 
+        self.init_window()
+        # load scrolling background from other .py
+    
+    def load_dino(self): 
+        self.dino = tk.PhotoImage(file = "../stimuli/dino.png")
+
+    def init_window(self): 
+        pass
+    def avoid_obstacles(self): 
+        pass
+        # load obstacles 
+
+
+
+
+def main(): 
+     my_display = Display()
+     my_display.root.mainloop() 
+
+if __name__ == "__main__":
+    main()
